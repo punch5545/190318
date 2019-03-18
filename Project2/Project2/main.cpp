dd401ba99c9c662d2f450c2847698edd3fd2cf3e
punch5545 <punch5545@naver.com>
2019-03-19 02:06:58 +0900

    Update main.cpp

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <conio.h>
-
+#include <string.h>
 int strLength(char* str1);
-void strCmp(char* str1, char* str2);
+int strCmp(char* str1, char* str2);
 
 int main() {
 	char str1[255];
@@ -24,17 +24,23 @@ int main() {
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 			break;
 		case 2:
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
 			printf("Input First String : ");
 			scanf("%s", str1);
 			printf("Input Second String : ");
 			scanf("%s", str2);
 
-			strCmp(str1, str2);
+			printf("return : %d\n", strCmp(str1, str2));
 			break;
-		default:break;
+		default:printf("다시 입력\n");		break;
 
 			return 0;
 		}
+		printf("\nPress Any Key to Continue\n");
+		char key = getch();
+		_getch();
+		system("cls");
 	}
 }
 
@@ -49,24 +55,45 @@ int strLength(char* str1) {
 }
 
 
-void strCmp(char* str1, char* str2) {
+int strCmp(char* str1, char* str2) {
+	int ret = 0;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-	printf("First string : %s, %dbyte\n", str1, strLength(str1));
-	printf("Second string : %s, %dbyte\n", str2, strLength(str2));
-	if (strLength(str1) > strLength(str2)) {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-		printf("The first string is longer than second one.\n\n");
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
+	printf("첫번째 문자열의 아스키값 : ");
+	for (int i = 0; i < strLength(str1); i++) {
+		if (str1[i] != str2[i]) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+		}
+		printf("%d ", str1[i]);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 	}
-		
-	else if (strLength(str1) < strLength(str2)) {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-		printf("The second string is longer than first one.\n\n");
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	printf("\n");
+
+	printf("두번째 문자열의 아스키값 : ");
+	for (int i = 0; i < strLength(str2); i++) {
+		if (str1[i] != str2[i]) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+		}
+		printf("%d ", str2[i]);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 	}
-	else {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-		printf("It both strings are same length\n\n");
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	printf("\n");
+	if (strLength(str2) > strLength(str1))
+		ret = strLength(str2);
+	else
+		ret = strLength(str1);
+
+	for (int i = 0; i < ret; i++) {
+		if (str1[i] > str2[i])
+			return 1;
+		else if (str1[i] < str2[i])
+			return -1;
 	}
+	
+	if (str1[ret] > str2[ret])
+		return 1;
+	else if (str1[ret] < str2[ret])
+		return -1;
+	else
+		return 0;
 }
